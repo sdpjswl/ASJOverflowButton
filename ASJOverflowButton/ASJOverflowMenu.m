c1b7fa1c185397f7296a5c231a7ae8ae3e1050a2
Sudeep <sdpjswl1@gmail.com>
2022-01-26 03:37:38 +0530

    Modified logic for width constraints that handles deallocation of previous constraint

--- ASJOverflowButton/ASJOverflowMenu.m
+++ ASJOverflowButton/ASJOverflowMenu.m
@@ -365,9 +365,11 @@ static NSString *const kCellIdentifier = @"cell";
 {
     NSAssert(widthMultiplier >= 0.0f && widthMultiplier <= 1.0f, @"Width multiplier must range from 0 to 1.");
     _widthMultiplier = widthMultiplier;
-    _widthConstraint.active = NO;
-    _widthConstraint = self.widthConstraintForMultiplier;
-    _widthConstraint.active = YES;
+    
+    NSLayoutConstraint *c = self.widthConstraintForMultiplier;
+    [NSLayoutConstraint deactivateConstraints:@[_widthConstraint]];
+    _widthConstraint = c;
+    [NSLayoutConstraint activateConstraints:@[_widthConstraint]];
 }
 
 - (NSLayoutConstraint *)widthConstraintForMultiplier
